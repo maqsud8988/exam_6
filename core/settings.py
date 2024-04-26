5dee0464338d50195dac3b0ef46294de4daf2f5f
maqsud <sunnatovmaqsud175@gmail.com>
2024-04-26 07:02:55 +0500

    localhost

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
